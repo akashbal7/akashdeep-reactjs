d25c179393569c5f0914de5eb36198d763f7fedf
Akashdeep Singh <singhakash0796@gmail.com>
2024-10-20 00:42:47 -0400

    add some changes in signup page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <header>
       <div className="header-contents">
-        <h2>Order Your Favourite Food Here</h2>
-        <p>
+        <h2  className="text-blue-600">Order Your Favourite Food Here</h2>
+        <p className="text-4xl">
           Choose from a diverse menu featuring a delectable array of dishes
           crafted with the finest ingredients and culinary expertise. Our
           mission is to satisfy your cravings and elevate your dining
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -47,7 +47,7 @@ const Register = ({ setShowLogin, setShowRegister }) => {
           </div>
         </div>
 
-        <button>Create Account</button>
+        <button className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Create Account</button>
 
         <div className="login-popup-condition">
           <input type="checkbox" required />
